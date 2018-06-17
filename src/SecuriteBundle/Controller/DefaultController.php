1bda8ada7d6b36b91ac675d6d8fa57f9542c5045
roche <roche@roche-XPS-13-1943.com>
2018-06-17 17:43:30 +0200

    modifier login controller

--- src/SecuriteBundle/Controller/DefaultController.php
+++ src/SecuriteBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace SecuriteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,14 @@ class DefaultController extends Controller
     public function loginAction()
     {
         return $this->render('securite/login.html.twig');
+    }
+
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction(Request $request)
+    {
+        throw new \RuntimeException('This should never be called directly');
     }
 }
