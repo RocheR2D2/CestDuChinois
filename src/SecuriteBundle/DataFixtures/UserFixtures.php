abc9ba2746724a90ad8431812bfd136a60f14411
roche <roche@roche-XPS-13-1943.com>
2018-06-19 14:03:06 +0200

    ajouter user data fixture

--- src/SecuriteBundle/DataFixtures/UserFixtures.php
+++ src/SecuriteBundle/DataFixtures/UserFixtures.php
@@ -9,7 +9,36 @@
 namespace SecuriteBundle\DataFixtures;
 
 
-class UserFixtures
-{
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use SecuriteBundle\Entity\User;
 
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager) {
+
+        $user1 = new User();
+        $user1->setFirstname('Julia');
+        $user1->setLastname('NVN');
+        $user1->setSex('F');
+        $user1->setEmail('julia.navennec@hotmail.com');
+        $user1->setPassword('$2y$13$vbonEYxHT6T9iSF1ePGQ3uwDMXJ7L7DSQsnqKiyjad6mDpA1yaQHq');
+        $user1->setUsername('julianvn');
+        $user1->setRoles(['ROLE_USER']);
+
+
+        $user2 = new User();
+        $user2->setFirstname('Haoshi');
+        $user2->setLastname('CHEN');
+        $user2->setSex('M');
+        $user2->setEmail('rocher2d2@gmail.com');
+        $user2->setPassword('$2y$13$vbonEYxHT6T9iSF1ePGQ3uwDMXJ7L7DSQsnqKiyjad6mDpA1yaQHq');
+        $user2->setUsername('rocher2d2');
+        $user2->setRoles(['ROLE_ADMIN']);
+
+
+        $manager->persist($user1);
+        $manager->persist($user2);
+        $manager->flush();
+    }
 }
