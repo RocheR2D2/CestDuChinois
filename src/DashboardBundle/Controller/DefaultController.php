3708bf60dd75a877ef4546ee4fab6f92700d5737
roche <roche@roche-XPS-13-1943.com>
2018-06-19 14:03:25 +0200

    maj dashboard Controller

--- src/DashboardBundle/Controller/DefaultController.php
+++ src/DashboardBundle/Controller/DefaultController.php
@@ -4,14 +4,18 @@ namespace DashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/dashboard")
+     * @Route("/dashboard", name="dashboard")
      */
-    public function dashboardAction()
+    public function dashboardAction(Request $request)
     {
-        return $this->render('dashboard/index.html.twig');
+        $username = $this->getUser();
+        return $this->render('dashboard/index.html.twig', [
+            'username' => $username
+        ]);
     }
 }
