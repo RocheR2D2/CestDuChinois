dfca0fa771f0f1a28f5c8ac55e2163d755f6d5d3
roche <roche@roche-XPS-13-1943.com>
2018-07-08 20:30:29 +0200

    modifier Tags Fixtures files

--- src/RecetteBundle/DataFixtures/TagsFixtures.php
+++ src/RecetteBundle/DataFixtures/TagsFixtures.php
@@ -9,7 +9,27 @@
 namespace RecetteBundle\DataFixtures;
 
 
-class TagsFixtures
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use RecetteBundle\Entity\Tags;
+
+class TagsFixtures extends Fixture
 {
 
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+
+            $tag = new Tags();
+            $tag->setName("tag".$i);
+            $manager->persist($tag);
+        }
+
+        $manager->flush();
+    }
 }
