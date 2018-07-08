332b15e4db7fdac3328aa459bad3b1e8bab7152f
roche <roche@roche-XPS-13-1943.com>
2018-07-08 20:29:12 +0200

    ajotuer les forms  des recetees

--- src/RecetteBundle/Form/EtapeType.php
+++ src/RecetteBundle/Form/EtapeType.php
@@ -9,7 +9,28 @@
 namespace RecetteBundle\Form;
 
 
-class EtapeType
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EtapeType extends AbstractType
 {
 
+    public function buildForm(FormBuilderInterface $builder, array $osptions)
+    {
+        $builder
+            ->add('etapeImg', FileType::class, [
+                'attr'=>[
+                    'class' => 'form-control',
+                    'name' => 'etapeImg[]',
+                ]
+            ])
+            ->add('etapeDescription', TextType::class, [
+                'attr'=>[
+                    'class' => 'form-control',
+                    'name' => 'etapeDescription[]',
+                ]
+            ]);
+    }
 }
--- src/RecetteBundle/Form/RecetteType.php
+++ src/RecetteBundle/Form/RecetteType.php
@@ -2,31 +2,119 @@
 
 namespace RecetteBundle\Form;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RecetteType extends AbstractType
 {
-    /**
-     * {@inheritdoc}
-     */
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $fontcolor = 'color:#3B3B3B;';
+
         $builder
-            ->add('nom')
-            ->add('description')
-            ->add('etape')
-            ->add('ingredient')
-            ->add('saveur')
-            ->add('facons')
-            ->add('temps')
-            ->add('niveau')
-            ->add('valide')
-            ->add('status');
-    }/**
-     * {@inheritdoc}
-     */
+            ->add('titre', TextType::class, [
+                'label' => 'Titre',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'name' => 'titre',
+                ]
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'name' => 'image',
+                ]
+            ])
+            ->add('description', TextType::class,[
+                'label' => 'Description',
+                'attr'=>[
+                    'name' => 'description',
+                    'class' => 'form-control',
+                ]
+            ])
+
+            ->add('etape', EtapeType::class, [
+                'label' => 'Étape',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'style' => 'display:none',
+                    'name' => 'etape[]',
+                ]
+            ])
+
+            ->add('ingredient', TextType::class, [
+                'label' => 'Ingrédients',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'name' => 'ingredient[]',
+                ]
+            ])
+            ->add('saveur', ChoiceType::class, [
+                'label' => 'Saveurs',
+                'attr'=>[
+                    'name' => 'saveur',
+                    'style' => $fontcolor
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'choices'  => [
+                    'Sucré ' => 0,
+                    'Salé ' => 1,
+                    'Acide ' => 2,
+                    'Amer ' => 3,
+                ],
+
+            ])
+            ->add('facons', ChoiceType::class, [
+                'label' => 'Manière de Cuisine',
+                'attr'=>[
+                    'name' => 'facon',
+                    'style' => $fontcolor
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'choices'  => [
+                    'fri' => 0,
+                    'sauté' => 1,
+                    'à vapeur' => 2,
+                    'pression' => 3,
+                ]
+            ])
+            ->add('temps', TextType::class, [
+                'label' => 'Temps de Cuission',
+                'attr'=>[
+                    'class' => 'form-control',
+                    'name' => 'temps',
+                ]
+            ])
+            ->add('niveau', ChoiceType::class, [
+                'label' => 'Difficulté',
+                'attr'=>[
+                    'name' => 'niveau',
+                ],
+                'choices'  => [
+                    'facile' => 0,
+                    'moyen' => 1,
+                    'difficile' => 2,
+                ],
+            ])
+            ->add('tags', TagsType::class);
+
+
+            //->getForm();
+    }
+
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
@@ -34,13 +122,11 @@ class RecetteType extends AbstractType
         ));
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    /*
     public function getBlockPrefix()
     {
         return 'recettebundle_recette';
     }
-
+    */
 
 }
