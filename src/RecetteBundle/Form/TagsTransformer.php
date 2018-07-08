db186242ab0f148b8d52ce52259c6362ac72fa8f
roche <roche@roche-XPS-13-1943.com>
2018-07-08 20:24:39 +0200

    ajouter tagstype et datatransformer

--- src/RecetteBundle/Form/TagsTransformer.php
+++ src/RecetteBundle/Form/TagsTransformer.php
@@ -9,7 +9,58 @@
 namespace RecetteBundle\Form;
 
 
-class TagsTransformer
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\PersistentCollection;
+use RecetteBundle\Entity\Tags;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class TagsTransformer implements DataTransformerInterface
 {
 
+    /**
+     *
+     * @param mixed $value The value in the original representation
+     *
+     * @return mixed The value in the transformed representation
+     *
+     * @throws TransformationFailedException when the transformation fails
+     */
+    public function transform($array)
+    {
+
+        $newArray = array();
+
+        if (!($array instanceof PersistentCollection)) {
+            dump($array->toArray());
+            return new ArrayCollection();
+        }
+
+
+        foreach ($array as $key => $value) {
+            $newArray[] = $value;
+        }
+
+
+        dump($newArray);
+        return new ArrayCollection($newArray);
+
+
+    }
+
+
+    public function reverseTransform($value)
+    {
+        // TODO: Implement reverseTransform() method.
+        $tagname =  explode(',', $value);
+        $tags = [];
+        foreach($tagname as $name) {
+            $tag = new Tags();
+            $tag->setName($name);
+            $tags[] = $tag;
+
+        }
+
+        return $tags;
+    }
 }
--- src/RecetteBundle/Form/TagsType.php
+++ src/RecetteBundle/Form/TagsType.php
@@ -9,7 +9,43 @@
 namespace RecetteBundle\Form;
 
 
-class TagsType
+use RecetteBundle\Entity\Recette;
+use RecetteBundle\Entity\Tags;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+
+class TagsType extends AbstractType
 {
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        //$builder->add('tags', TextType::class);
+
+        /*
+        $builder
+            //->addModelTransformer(new CollectionToArrayTransformer(),true)
+            ->addModelTransformer(new TagsTransformer(), true);
+        */
+
+        $builder
+            //->addModelTransformer(new CollectionToArrayTransformer(),true)
+            ->addModelTransformer(new TagsTransformer());
+
+
+    }
+
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
 
 }
