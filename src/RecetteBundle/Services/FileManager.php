cbc9597c8ba306c8f37cae798cb1565accdce859
haoshichen <haoshichen@outlook.fr>
2018-07-11 17:41:39 +0200

    ajouter la service FileManager

--- src/RecetteBundle/Services/FileManager.php
+++ src/RecetteBundle/Services/FileManager.php
@@ -6,7 +6,33 @@
  * Time: 14:21
  */
 
+namespace RecetteBundle\Services;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class FileManager
 {
 
+    private $directory;
+
+    public function __construct(Twig $twig, string $directory)
+    {
+        $this->directory = $directory;
+    }
+
+
+    public function uploadFile(UploadedFile $uploadedFile) {
+        $filename = md5(uniqid()) . '.' . $uploadedFile->guessExtension();
+
+        $uploadedFile->move(
+            $this->directory,
+            $filename
+        );
+
+
+        return $filename;
+
+
+    }
 }
