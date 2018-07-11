b4b8c6d4cd8bfbe74a5d678c5e123632e6153eba
roche <roche@roche-XPS-13-1943.com>
2018-07-11 11:28:54 +0200

    Modification des Form

--- src/RecetteBundle/Form/EtapeType.php
+++ src/RecetteBundle/Form/EtapeType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EtapeType extends AbstractType
 {
@@ -20,17 +21,23 @@ class EtapeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $osptions)
     {
         $builder
-            ->add('etapeImg', FileType::class, [
+            ->add('etapeImage', FileType::class, [
                 'attr'=>[
-                    'class' => 'form-control',
-                    'name' => 'etapeImg[]',
+                    'required' => false,
+                    'data_class' => null
                 ]
             ])
-            ->add('etapeDescription', TextType::class, [
-                'attr'=>[
-                    'class' => 'form-control',
-                    'name' => 'etapeDescription[]',
-                ]
-            ]);
+            ->add('etapeDescription', TextType::class);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RecetteBundle\Entity\Etape'
+        ));
     }
 }
