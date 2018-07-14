57b51bc546a5eaae3710298ec32e2477085a6c72
haoshichen <haoshichen@outlook.fr>
2018-07-14 17:43:52 +0200

    ajouter Province Entity

---
+++ src/RecetteBundle/Entity/Province.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace RecetteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Province
+ *
+ * @ORM\Table(name="province")
+ * @ORM\Entity(repositoryClass="RecetteBundle\Repository\ProvinceRepository")
+ */
+class Province
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uid", type="string", length=255, unique=true)
+     */
+    private $uid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Province
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    /**
+     * @param string $uid
+     */
+    public function setUid($uid)
+    {
+        $this->uid = $uid;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+}
+
